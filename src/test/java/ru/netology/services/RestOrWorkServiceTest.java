350342cfeffcb4af16c595f87ec54fb7a9e8d2a8
Netology <orjuice@mail.ru>
2023-04-12 20:59:00 +0300

    First commit

--- src/test/java/ru/netology/services/RestOrWorkServiceTest.java
+++ src/test/java/ru/netology/services/RestOrWorkServiceTest.java
@@ -2,22 +2,25 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class RestOrWorkServiceTest {
-    @Test
-    public void CalculateRestOrWork() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/figures.csv")
+    public void CalculateRestOrWork(int expected, int income, int expense, int threshold) {
         RestOrWorkService service = new RestOrWorkService();
-        int expected = 3;
-        int actual = service.calculate(10000, 3000, 20000);
+        //int expected = 3;
+        int actual = service.calculate(income, expense, threshold);
 
     }
 
-    @Test
-    public void CalculateRestOrWork2() {
-        RestOrWorkService service = new RestOrWorkService();
-        int expected = 2;
-        int actual = service.calculate(100000, 60000, 15000);
-    }
+    //@Test
+    //public void CalculateRestOrWork2() {
+    //RestOrWorkService service = new RestOrWorkService();
+    //int expected = 2;
+    //int actual = service.calculate(100000, 60000, 15000);
+    //}
 
 }
 
